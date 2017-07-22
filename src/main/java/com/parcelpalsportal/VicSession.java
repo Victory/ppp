1afe3ce63e031c89910dcae9bcab7441785bfb20
Victory <git@dfhu.org>
2017-07-22 15:15:22 -0400

    Use sparkingrocks (but haven't yet taken out local versions of classes)

--- src/main/java/com/parcelpalsportal/VicSession.java
+++ src/main/java/com/parcelpalsportal/VicSession.java
@@ -4,7 +4,11 @@ import spark.Request;
 import spark.Response;
 
 public class VicSession {
-  public VicSession(Request request, Response response) {
+  private final Request request;
+  private final Response response;
 
+  public VicSession(Request request, Response response) {
+   this.request = request;
+   this.response = response;
   }
 }
